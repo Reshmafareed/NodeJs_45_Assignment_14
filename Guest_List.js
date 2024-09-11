bafe0aec3cfd05e09b111219b73929e5688c3d60
Reshmafareed <163650782+Reshmafareed@users.noreply.github.com>
2024-09-11 12:00:38 +0500

    Add files via upload

---
+++ Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['Imran Khan', 'Nawaz Shareef', 'Asif Ali Zardari'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\n It is our pleasure to invite you in our party.\n\nThank You!');
+}
---
+++ Guest_List.ts
@@ -0,0 +1,6 @@
+let Guest_List: string[]= ['Imran Khan','Nawaz Shareef','Asif Ali Zardari'];
+
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Dear Mr. ' + Guest_List[i]+ ',\n\n It is our pleasure to invite you in our party.\n\nThank You!')
+
+}
